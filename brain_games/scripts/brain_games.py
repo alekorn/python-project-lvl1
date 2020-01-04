6eeb317fc5a07957d3f5497627a5dcd4e8514b61
alekorn <entkorn@gmail.com>
2020-01-04 23:32:23 +0300

    add flake8 and fix linter warnings

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 from brain_games.cli import run
 
+
 def greet():
     print('Welcome to the Brain Games!')
     print()
+
 
 def main():
     greet()
     run()
 
+
 if __name__ == '__main__':
     main()
-
