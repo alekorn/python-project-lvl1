fe0de96426fb4afe9fcb13020cbd54e0cd4952e1
alekorn <entkorn@gmail.com>
2020-01-11 22:46:03 +0300

    rename variables

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -3,17 +3,17 @@ import prompt
 
 def game_engine(RULE, game_logic):
     print('', 'Welcome to the Brain Games!', RULE, "", sep='\n')
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!\n')
+    user_name = prompt.string('May I have your user_name? ')
+    print(f'Hello, {user_name}!\n')
     for game_round in range(3):
-        question, answer = game_logic()
-        user_answer = prompt.string(f'Question: {question}\nYour answer: ')
-        if answer == user_answer:
+        game_task, correct_answer = game_logic()
+        user_answer = prompt.string(f'Question: {game_task}\nYour correct_answer: ')
+        if user_answer == correct_answer:
             print('Correct!')
         else:
-            print(f'\'{user_answer}\' is wrong answer ;(. '
-                  f'Correct answer was \'{answer}\'.\n'
-                  f'Let\'s try again, {name}!')
+            print(f'\'{user_answer}\' is wrong correct_answer ;(. '
+                  f'Correct correct_answer was \'{correct_answer}\'.\n'
+                  f'Let\'s try again, {user_name}!')
             break
     else:
-        print(f'Congratulations, {name}!')
+        print(f'Congratulations, {user_name}!')
