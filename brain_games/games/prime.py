c0dc912c139b2be473e9e604620c4e2168980023
alekorn <entkorn@gmail.com>
2020-01-11 13:08:45 +0300

    add prime game

---
+++ brain_games/games/prime.py
@@ -0,0 +1,17 @@
+import random
+
+
+def rule():
+    return 'Answer "yes" if number even otherwise answer "no".'
+
+
+def game_question():
+    question = random.randint(1, 99)
+    prime_ints = (
+        2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+        43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97)
+    if question in prime_ints:
+        answer = 'yes'
+    else:
+        answer = 'no'
+    return question, answer
