e2d52332928a5a848089dd376b22fea86b3678aa
alekorn <entkorn@gmail.com>
2020-01-11 19:57:39 +0300

    fix pep8 warnings

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -9,7 +9,8 @@ def game_question():
     question = random.randint(1, 99)
     prime_ints = (
         2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
-        43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97)
+        43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+    )
     if question in prime_ints:
         answer = 'yes'
     else:
