137b8e3fdda92d10a45d391c8cb32ff9feaf3d4f
alekorn <entkorn@gmail.com>
2020-01-18 09:10:31 +0300

    change is_prime func

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -3,10 +3,6 @@ import random
 RULE = 'Answer "yes" if given number is prime. Otherwise correct_answer "no".'
 START_RANGE = 1
 END_RANGE = 99
-PRIME_INTS = (
-        2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
-        43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
-    )
 
 
 def generate_round():
@@ -19,4 +15,12 @@ def generate_round():
 
 
 def is_prime(integer):
-    return integer in PRIME_INTS
+    if (integer == 1):
+        return False
+    elif (integer == 2):
+        return True
+    else:
+        for i in range(2, integer):
+            if(integer % i == 0):
+                return False
+        return True
